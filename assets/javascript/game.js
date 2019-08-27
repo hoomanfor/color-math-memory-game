34c602da60a7d8bf2dfda4f2ac96f34a6d416930
Hooman Foroudastan <hforoudastan@hforoudastan-wifi.rdu.lulu.com>
2019-08-27 16:24:06 -0400

    removed comments from game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,3 @@
-// At the start of the game, a random number is selected by the app and displayed on the DOM
-// At the start of the game, the user's score is 0 and displayed on the DOM
-// There are 4 crystals that each represent a unique number (The number value is unknown to the user).
-
-// When the user matches his score to the random number, he wins. The number of Wins is displayed on the DOM.
-// If the user scores higher than the randome number, he losses. The number of Losses is displayed on the DOM. 
-// The crystal values are a random number between 1 and 12.
-// The number I am trying to guess is between 19 and 120.
-
 $(document).ready(function() {
 
     var winningNumber = Math.floor(Math.random() * 120) + 19; //random number between 19 - 120
